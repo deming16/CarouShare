95e431f925fd3646cd10fe5707e9e8969eabef4a
deming16 <neverggyay@gmail.com>
2019-04-05 23:55:53 +0800

    add CRUD queries for item

--- app/routes/item.js
+++ app/routes/item.js
@@ -1,32 +1,64 @@
 var express = require('express');
 var router = express.Router();
 
+const db = require('../db');
+
 // @route   GET item/:itemId
 // @desc    Get a item
 // @access  Public
 router.get('/:itemId', (req, res, next) => {
-  res.send(`Get info for item ${req.params.itemId}`);
+  const query = "select * from Items where iid = $1"
+  const values = [req.params.itemId];
+
+  db.query(query, values)
+    .then(result => {
+      if (result.rows.length == 0) {
+        res.send('no such item');
+      }
+      else {
+        res.json(result.rows[0]);
+      }
+    })
+    .catch(err => {
+      res.render('error', { error: err, message: 'something went wrong' });
+    });
 });
 
 // @route   POST item/
 // @desc    Add new item
 // @access  Private
 router.post('/', (req, res, next) => {
-  res.send(`Item added`);
+  const query = "insert into Items (owner_uid, item_name, category, status, photo, description) " +
+    "values ($1, $2, $3, $4, $5, $6)"
+  const values = ['user', 'test', 'test', 'status', 'photo', 'desc'];
+
+  db.query(query, values)
+    .then(() => res.send('item added'))
+    .catch(err => res.render('error', { error: err, message: 'something went wrong' }));
 });
 
 // @route   POST item/:itemId
 // @desc    Update item
 // @access  Private
 router.post('/:itemId', (req, res, next) => {
-  res.send(`Item ${req.params.itemId} updated`);
+  const query = "update Items set item_name = $1, category = $2, status = $3, photo = $4, description = $5 where iid = $6"
+  const values = ['updatedName', 'category', 'status1', 'photo1', 'newDesc', req.params.itemId];
+
+  db.query(query, values)
+    .then(() => res.send('item updated'))
+    .catch(err => res.render('error', { error: err, message: 'something went wrong' }));
 });
 
 // @route   POST item/:itemId/delete
 // @desc    Delete item
 // @access  Private
 router.post('/:itemId/delete', (req, res, next) => {
-  res.send(`Item ${req.params.itemId} deleted`);
+  const query = "delete from Items where iid = $1"
+  const values = [req.params.itemId];
+
+  db.query(query, values)
+    .then(() => res.send('item deleted'))
+    .catch(err => res.render('error', { error: err, message: 'something went wrong' }));
 });
 
 // @route   POST item/:itemId/like
