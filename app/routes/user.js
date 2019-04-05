4db4bd5491b891b5bd9790b66b0ea39e7438fc40
deming16 <neverggyay@gmail.com>
2019-04-05 21:10:02 +0800

    add queries for user.js

--- app/routes/user.js
+++ app/routes/user.js
@@ -10,8 +10,9 @@ const db = require('../db');
 // @access  Private
 router.get('/', (req, res, next) => {
     if (req.isAuthenticated()) {
-        const query = 'SELECT uid, email, address, mobile FROM Users WHERE uid = $1::text';
-        db.query(query, [req.user.username])
+        const query = 'SELECT uid, email, address, mobile FROM Users WHERE uid = $1';
+        const values = [req.user.username];
+        db.query(query, values)
             .then(result => {
                 res.json(result.rows[0]);
             });
@@ -26,7 +27,9 @@ router.get('/', (req, res, next) => {
 // @desc    Get info on one user
 // @access  Public
 router.get('/:username', (req, res, next) => {
-    db.query('SELECT * FROM Users WHERE uid = $1::text', [req.params.username])
+    const query = 'SELECT * FROM Users WHERE uid = $1';
+    const values = [req.params.username];
+    db.query(query, values)
         .then(result => {
             if (result.rows.length === 0) {
                 res.send('user does not exist');
@@ -41,6 +44,9 @@ router.get('/:username', (req, res, next) => {
 // @desc    Update user
 // @access  Private
 router.post('/:username', (req, res, next) => {
+    const query = "UPDATE Users " +
+        "SET email = $1, address = $2, mobile = $3 " +
+        "WHERE uid = $4";
     res.send(`update ${req.params.username}`);
 });
 
@@ -48,6 +54,7 @@ router.post('/:username', (req, res, next) => {
 // @desc    Delete user
 // @access  Private
 router.post('/:username/delete', (req, res, next) => {
+    const query = "DELETE FROM Users WHERE uid = $1";
     res.send(`delete ${req.params.username}`);
 });
 
@@ -55,6 +62,7 @@ router.post('/:username/delete', (req, res, next) => {
 // @desc    Add likes for user
 // @access  Private
 router.post('/:username/like', (req, res, next) => {
+    const query = "insert into UserLikeItems (user_uid, item_iid) values ($1, $2)";
     res.send(`Add like for ${req.params.username}`);
 });
 
@@ -62,6 +70,8 @@ router.post('/:username/like', (req, res, next) => {
 // @desc    Delete likes for user
 // @access  Private
 router.post('/:username/like/delete', (req, res, next) => {
+    const query = "delete from UserLikeItems where user_uid = $1"
+    const values = [req.params.username];
     res.send(`Delete like for ${req.params.username}`);
 });
 
