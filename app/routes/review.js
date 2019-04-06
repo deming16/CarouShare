1398aa2d80678cd60ed1698dcdab23547d119955
deming16 <neverggyay@gmail.com>
2019-04-06 13:20:06 +0800

    add query statements for review.js

--- app/routes/review.js
+++ app/routes/review.js
@@ -6,6 +6,8 @@ const db = require('../db');
 // @desc    Update review
 // @access  Private
 router.post('/:reviewId', (req, res, next) => {
+  const query = "update ReviewSections set content = $1 where review_rid = $2";
+  const values = ['newContent', req.params.reviewId];
   res.send(`Review ${req.params.reviewId} updated`);
 });
 
@@ -13,6 +15,8 @@ router.post('/:reviewId', (req, res, next) => {
 // @desc    Delete review
 // @access  Private
 router.post('/:reviewId/delete', (req, res, next) => {
+  const query = "delete from Reviews where rid = $1";
+  const values = [req.params.reviewId];
   res.send(`Review ${req.params.reviewId} deleted`);
 });
 module.exports = router;
